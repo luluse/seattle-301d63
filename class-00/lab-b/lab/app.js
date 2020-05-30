ef4745a7aff109c4a5b9fde0dbecd7c9f44d8b2d
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-05-29 17:58:54 -0700

    step 11

--- class-00/lab-b/lab/app.js
+++ class-00/lab-b/lab/app.js
@@ -214,17 +214,19 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// Joe.scope is part of a constructor function, and "this" refers to the new instance object called joe on line 177.
+
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// The arrow function doesn't reference the constructor function that defines "this"."this" will refer to enclosing scope since it is not present in the current scope, here the enclosing score is the window browser.
+
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// Arrow functions cannot be used with constructors and same with prototype. An arrow function doesn't bind "this" and will raise an error or undefined.
